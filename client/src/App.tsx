66b3db4e68f17c3fb668fa5b12bcc67c39956412
Amadeus <igniferwargus@gmail.com>
2023-07-26 00:57:09 +0300

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <label className="text-3xl font-bold underline stroke-slate-300">sss</label>
+    </div>
+  );
+}
+
+export default App;
